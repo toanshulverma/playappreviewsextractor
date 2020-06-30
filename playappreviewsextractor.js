1ba153aaed271ff70a6604863060c41f2bf1140f
= <=>
2020-06-30 01:15:40 -0500

    memory optimized - remove processed nodes

--- playappreviewsextractor.js
+++ playappreviewsextractor.js
@@ -2,13 +2,19 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 function run () {
+
     return new Promise(async (resolve, reject) => {
+        
         try {
-            const browser = await puppeteer.launch();
+
+            var start = new Date()
+            var hrstart = process.hrtime();
+            
+            const browser = await puppeteer.launch({ dumpio : false});
             const page = await browser.newPage();
 
-            const hop_delay = 1500;
-            const max_Hops = 10000;
+            const hop_delay = 1000;
+            const max_Hops = 50;
             const max_no_progress_attempt = 5;
 
             var scrlheight = -1;
@@ -35,30 +41,15 @@ function run () {
             
             while( pageCount <= max_Hops && no_progress_attempt < max_no_progress_attempt){
                 
-                page.waitFor(100);  //added delayd for page load/ paint
+                //page.waitFor(100);  //added delayd for page load/ paint
 
                 //increment counter if no results retreived, else reset counter to 0
                 no_progress_attempt = (scrlheight == page_height) ? (no_progress_attempt + 1) : 0;
 
+                console.log('---------- scrlheight  = ' + scrlheight);
+                console.log('---------- page_height = ' + page_height);
 
-                scrlheight = page_height;
-
-                await page.evaluate(() => {
-                    
-                    var showMorebutton = document.querySelectorAll(".RveJvd");
-
-                    // if "show more" button is displayed, invoke click
-                    if(showMorebutton && showMorebutton.length > 0 && showMorebutton[0].innerText === "SHOW MORE"){
-                        showMorebutton[0].click();  //click on show more button
-                    }
-                    else{
-                        window.scrollTo(0,document.body.scrollHeight);      //infinity scroll
-                    }
-                });
-                
                 //add delay for additional data load
-                await page.waitFor(hop_delay);
-                 
                 pageCount++;
 
                 //extract data
@@ -71,11 +62,11 @@ function run () {
                     var lstDates = document.querySelectorAll(".kx8XBd > div > .p2TkOb");
                     var lstComments = document.querySelectorAll(".bAhLNe > .UD7Dzf > span:first-child");
                     var lstRatings = document.querySelectorAll(".kx8XBd > div > .nt2C1d > .pf5lIe > div");
-                
+
                     let csvContent = ""; 
                     
                     //restart work from last batch
-                    for(var i = window.processedItems; i < lstLblNames.length; i++){
+                    for(var i = 0; i < lstLblNames.length; i++){
                         /*csvContent += sanitizeString(lstLblNames[i].innerText) + "," 
                                         + lstRatings[i].querySelectorAll(".vQHuPe").length + "," 
                                         + sanitizeString(lstDates[i].innerText) + "," 
@@ -92,9 +83,16 @@ function run () {
 
                         window.processedItems++;
                     }
-                    
+
+                    // remove extracted (processed) nodes to save memory
+                    document.querySelectorAll("[jsname=fk8dgd]").forEach( function(item){ item.innerHTML = '' });
+
                     return csvContent;
                 });
+
+                // get new page height (before reload of page)
+                var bodyHeight = await page.evaluateHandle(() => document.body.scrollHeight);
+                scrlheight = await bodyHeight.jsonValue();
 
                 if(csvContent.length > 0){
                     //write file for each hop to save intermediate work
@@ -106,9 +104,26 @@ function run () {
                     });
                 }
 
-                 // get new page height
-                 var bodyHeight = await page.evaluateHandle(() => document.body.scrollHeight);
-                 page_height = await bodyHeight.jsonValue();
+                await page.evaluate(() => {
+                    
+                    var showMorebutton = document.querySelectorAll(".RveJvd");
+
+                    // if "show more" button is displayed, invoke click
+                    if(showMorebutton && showMorebutton.length > 0 && showMorebutton[0].innerText === "SHOW MORE"){
+                        showMorebutton[0].click();  //click on show more button
+                    }
+                    else{
+                        // handle infinity scroll
+                        window.scrollTo(0,0);                               // scroll to top
+                        window.scrollTo(0,document.body.scrollHeight);      // scroll to bottom
+                    }
+                });
+
+                await page.waitFor(hop_delay);
+
+                // get new page height (after content reload)
+                var bodyHeight = await page.evaluateHandle(() => document.body.scrollHeight);
+                page_height = await bodyHeight.jsonValue();
             }
 
             // show informative message if process exited due to no more content
@@ -116,11 +131,20 @@ function run () {
                 console.log('Exiting as no more page loads available');
             }
 
+            // end browsing session
             browser.close();
+
+            // calculate and display overall processing time
+            var end = new Date() - start,
+            hrend = process.hrtime(hrstart);
+            console.info('Execution time: %dms', end)
+            console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
+            
+
             return resolve();
         } catch (e) {
             return reject(e);
         }
-    })
+    });
 }
 run().then(console.log).catch(console.error);
